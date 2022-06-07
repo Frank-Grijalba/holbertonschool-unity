ad0e6e979ad4357414493f920cdab988f3f6b706
Frank-Grijalba <fjgrijalba@gmail.com>
2022-06-06 20:47:34 -0500

    Move the player in the vector pos x and z

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,45 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-using System.Xml.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    // variables for x-axis and y-axis, x = horizontal, z = vertical
-    private float Horizontal;
+    public float horizontalMove;
+    public float verticalMove;
+    public CharacterController player;
 
-    private float Vertical;
-    public float speed;
-    public float jump;
-    private float gravityValue = 9.81f;
-
-    private UnityEngine.CharacterController player;
-
-    void Start ()
+    public float playerSpeed;
+    // Start is called before the first frame update
+    void Start()
     {
-        player = GetComponent<CharacterController> ();
+        player = GetComponent<CharacterController>();
+        
     }
 
-    void update ()
+    // Update is called once per frame
+    void Update()
     {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        player.Move((new Vector3(horizontalMove, 0, verticalMove)) * playerSpeed * Time.deltaTime);
     }
-
-
-    void FixedUpdate()
-    {
-        // GetAxis take the value of the mouse too,Typically a positive value means the mouse is moving 
-        // right/down and a negative value means the mouse is moving left/up.
-        //Input.GetAxis("Mouse X") and  Input.GetAxis("Mouse Y") to move in the vector in X and Z
-        Horizontal = Input.GetAxis("Horizontal");
-        Vertical = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(Horizontal, 0.0f, Vertical);
-        player.Move(move * speed * Time.deltaTime);
-
-        // value keyboard Space
-        if (Input.GetKey(KeyCode.Space))
-        {
-            player.Move(new Vector3(0, gravityValue * jump * Time.deltaTime, 0));
-        }
-    }
-} 
+}
